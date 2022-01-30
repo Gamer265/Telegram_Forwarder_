5132d13be374dd1df9f2abb8b0cb9b08fdf12f73
Coder-265 <93988223+Coder-265@users.noreply.github.com>
2022-01-30 14:07:46 +0530

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -2,7 +2,7 @@ from forwarder.sample_config import Config
 
 
 class Development(Config):
-    API_KEY = "5227462308:AAGxHrd_77vtCo9vuZ9AacI8p3gU5ijE4io"  # Your bot API key
+    API_KEY = "5076883469:AAGCLYTEaE_4mcJZhbP5ja3FNqqmwU_NMKI"  # Your bot API key
     OWNER_ID = 862271564  # Your user id
 
     # Make sure to include the '-' sign in group and channel ids.
