172e04dd77175fe9d7c6289c5c08aa6ebdca856e
Gamer265 <89581107+Gamer265@users.noreply.github.com>
2022-01-31 20:11:10 +0530

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -10,7 +10,7 @@ class Development(Config):
     FROM_CHATS = [-1001728410290]# List of chat id's to forward messages from.
     TO_CHATS = [-1001469717002]# List of chat id's to forward messages to.
 
-    REMOVE_TAG = False
+    REMOVE_TAG = True
     WORKERS = 16
    
     
